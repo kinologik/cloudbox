f1b2248f2056438a6f479e53261cb643a46018df
kinologik <martin@kinologik.com>
2015-04-27 00:24:36 -0400

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 {
+CBURL='https://raw.githubusercontent.com/kinologik/cloudbox/master'
 ln -s /etc/environment ${HOME}/.ssh/environment
 
 SYSTEMD=$(pidof systemd)
+
 if [[ -z ${SYSTEMD} ]]; then
 	cp /etc/inittab /etc/inittab.backup
 	curl -o /etc/inittab ${CBURL}/etc/inittab
@@ -12,7 +14,7 @@ TTYLOGIN='/etc/systemd/system/getty@tty1.service.d/autologin.conf'
 curl --create-dirs -o ${TTYLOGIN} ${CBURL}$(echo ${TTYLOGIN} | sed 's|@|-|g')
 
 curl -o ${HOME}/.bashrc ${CBURL}/root/.bashrc.01
-rm ${HOME}/init.sh
+rm ${HOME}/start.sh
 reboot
 
 exit
