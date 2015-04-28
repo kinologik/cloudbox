516442af6205460918bfc1ed95cb154a1e6d4dd5
kinologik <martin@kinologik.com>
2015-04-28 09:37:11 -0400

    Update lib.sh

--- root/.scripts/lib.sh
+++ root/.scripts/lib.sh
@@ -8,3 +8,10 @@ backup() {
 	mkdir -p ${BCKUP}${BCKDIR}
     	mv ${1} ${BCKUP}${1}
 }
+
+copy() {
+	BCKDIR=$(dirname ${1})
+	BCKFILE=$(basename ${1})
+	mkdir -p ${BCKUP}${BCKDIR}
+    	cp ${1} ${BCKUP}${1}
+}
