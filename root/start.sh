81dfbb891eb98e789cd8970277bd522f172eee54
kinologik <martin@kinologik.com>
2015-04-28 08:28:41 -0400

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -15,7 +15,7 @@ curl --create-dirs -o ${TTYLOGIN} ${CBURL}$(echo ${TTYLOGIN} | sed 's|@|-|g')
 if [ $? != 0 ]; then echo "systemd autologin download failed..."; exit; fi
 
 backup ${HOME}/.bashrc
-curl -o ${HOME}/.bashrc ${CBURL}/root/.bashrc.01
-if [ $? != 0 ]; then echo ".bashrc download failed..."; exit; fi
-rm ${HOME}/start.sh
+# curl -o ${HOME}/.bashrc ${CBURL}/root/.bashrc.01
+# if [ $? != 0 ]; then echo ".bashrc download failed..."; exit; fi
+# rm ${HOME}/start.sh
 reboot
