87af350f255577c4b7ecd655bdaa51f956e3d921
kinologik <martin@kinologik.com>
2015-04-27 20:18:27 -0400

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -2,23 +2,38 @@
 
 {
 CBURL='https://raw.githubusercontent.com/kinologik/cloudbox/master'
+BCKUP=${HOME}'/.backup'
+
+alias mkdir="mkdir -p"
+
+backup() {
+	BCKDIR=$(dirname ${1})
+	BCKFILE=$(basename ${1})
+	mkdir ${BCKUP}${BCKDIR}
+    	mv ${1} ${BCKUP}${1}
+}
+
+mkdir --parents ${BCKUP}'/'{home,etc,var/{www/html/{localhost,*},run}}
 ln -s /etc/environment ${HOME}/.ssh/environment
 
 SYSTEMD=$(pidof systemd)
 
 if [[ -z ${SYSTEMD} ]]; then
-	cp /etc/inittab /etc/inittab.backup
-	curl -o /etc/inittab ${CBURL}/etc/inittab
+		# mv /etc/inittab ${BCKUP}/etc/inittab
+	backup /etc/inittab
+	# curl -o /etc/inittab ${CBURL}/etc/inittab
 	if [ $? != 0 ]; then echo "inittab download failed..."; exit; fi
 fi
+
 TTYLOGIN='/etc/systemd/system/getty@tty1.service.d/autologin.conf'
 curl --create-dirs -o ${TTYLOGIN} ${CBURL}$(echo ${TTYLOGIN} | sed 's|@|-|g')
 if [ $? != 0 ]; then echo "systemd autologin download failed..."; exit; fi
 
-curl -o ${HOME}/.bashrc ${CBURL}/root/.bashrc.01
+backup ${HOME}/.bashrc
+# curl -o ${HOME}/.bashrc ${CBURL}/root/.bashrc.01
 if [ $? != 0 ]; then echo ".bashrc download failed..."; exit; fi
 rm ${HOME}/start.sh
-reboot
+# reboot
 
 exit
 }
