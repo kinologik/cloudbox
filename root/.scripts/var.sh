9933e90dd10152faa313f188432dc0a4db421cbc
kinologik <martin@kinologik.com>
2015-07-21 16:44:31 -0400

    Create var.sh

---
+++ root/.scripts/var.sh
@@ -0,0 +1,11 @@
+CBURL='https://raw.githubusercontent.com/kinologik/cloudbox/master'
+
+BCKUP=${HOME}'/.backup'
+SCRIPTS=${HOME}'/.scripts'
+
+TRASHFILE='/root/.local/share/Trash/files'
+TRASHINFO='/root/.local/share/Trash/info'
+
+TTYLOGIN='/etc/systemd/system/getty.target.wants/getty@tty1.service'
+
+SYSLOG='/var/log/syslog'
