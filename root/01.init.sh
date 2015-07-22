60b4a667897d1313b28f41a4171222f6f54ddbff
kinologik <martin@kinologik.com>
2015-07-21 22:23:13 -0400

    Create 01.init.sh

---
+++ root/01.init.sh
@@ -0,0 +1,80 @@
+## OS UPDATE/UPGRADE, SWAPFILE, HOSTNAME + TIMEZONE + LOCALES SETTINGS && DELETE HISTORY ##
+
+source ${HOME}/.scripts/lib.sh
+
+function set_HTL
+{
+		backup /etc/hostname
+	echo -e ${HNAME} > /etc/hostname
+	sed -i 's|127.0.0.1 localhost|127.0.0.1 localhost '${HNAME}'|g' /etc/hosts
+		
+	#	backup /etc/timezone
+		copy /etc/timezone
+	# echo ${TZONE} > /etc/timezone
+		backup /etc/localtime
+	ln -s /usr/share/zoneinfo/${TZONE} /etc/localtime
+	# dpkg-reconfigure -f noninteractive tzdata
+		
+		backup /etc/default/locale
+	curl -o /etc/default/locale ${CBURL}/etc/default/locale
+	# dpkg-reconfigure -f noninteractive locales
+}
+
+function update_sources
+{
+		backup /etc/apt/sources.list
+	curl -o /etc/apt/sources.list ${CBURL}/etc/apt/sources.${OSDIST}.list
+	apt-get update
+	if [ $? != 0 ]; then echo "update failed..."; exit; fi
+	# curl -o /etc/apt/sources.list.d/experimental.list ${CBURL}/etc/apt/sources.list.d/experimental.list
+	# apt-get update
+}
+
+function update_upgrade
+{
+		backup /etc/apt/listchanges.conf
+	curl -o /etc/apt/listchanges.conf ${CBURL}/etc/apt/listchanges.conf
+	apt-get -y dist-upgrade
+	if [ $? != 0 ]; then echo "upgrade failed..."; exit; fi
+	apt-get -y autoremove
+}
+
+function create_swapfile
+{
+	touch /swapfile
+	chmod 600 /swapfile
+	dd if=/dev/zero of=/swapfile bs=1M count=2048
+	mkswap /swapfile
+	swapon /swapfile
+		copy /etc/fstab
+	echo '/swapfile none swap sw 0 0' >> /etc/fstab
+	sysctl -w vm.swappiness=30
+}
+
+if [ $(tty) == /dev/tty1 ]; then
+	## Set hostname, timezone && locale ##
+		set_HTL
+		
+	## Update Debian source files ##
+		update_sources
+
+	## Unattended Debian update/upgrade ##
+		update_upgrade
+
+	## Configure swapfile ##
+		create_swapfile
+
+	## SSH port settings ##
+			backup /etc/ssh/sshd_config
+		curl -o /etc/ssh/sshd_config ${CBURL}/etc/ssh/sshd_config
+		sed -i 's|{{port}}|'${SSHPORT}'|g' /etc/ssh/sshd_config
+
+	## Delete bash history && load next installation script ##
+		cat /dev/null > ${HOME}/.bash_history
+		history -c
+		curl -o ${HOME}/.bashrc ${CBURL}/root/.bashrc.02
+
+	## REBOOT ##
+		cat /dev/null > /var/log/syslog
+		# reboot
+fi
