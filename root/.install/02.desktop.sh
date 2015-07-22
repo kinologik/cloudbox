eb458d52eb608a5419b2b10b967ed7d9da3d21e2
kinologik <martin@kinologik.com>
2015-07-22 12:21:56 -0400

    Update 02.desktop.sh

--- root/.install/02.desktop.sh
+++ root/.install/02.desktop.sh
@@ -7,23 +7,16 @@ function install_xserver
 	apt-get -y --no-install-recommends install xbase-clients xserver-xorg
 	apt-get -y install xserver-xorg-video-dummy
 	apt-get -y install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic
-		# backup /etc/X11/xorg.conf
 	curl -o /etc/X11/xorg.conf ${CBURL}/etc/X11/xorg.conf
 }
 
 function install_openbox
 {
 	apt-get -y install libpam-gnome-keyring openbox menu obmenu x11vnc gtk2-engines gtk-chtheme hsetroot nitrogen
-	curl -o ${HOME}/.bashrc ${CBURL}/root/.bashrc
-		# backup ${HOME}/.xinitrc
-	curl -o ${HOME}/.xinitrc ${CBURL}/root/.xinitrc
 	curl --create-dirs -o ${HOME}/.scripts/emptyTrash.sh ${CBURL}/root/.scripts/emptyTrash.sh
 	chmod +x ${HOME}/.scripts/emptyTrash.sh
-		# backup ${HOME}/.config/openbox/autostart
 	curl --create-dirs -o ${HOME}/.config/openbox/autostart ${CBURL}/root/.config/openbox/autostart
-		# backup ${HOME}/.config/openbox/menu.xml
 	curl -o ${HOME}/.config/openbox/menu.xml ${CBURL}/root/.config/openbox/menu.xml
-		# backup ${HOME}/.config/openbox/rc.xml
 	curl -o ${HOME}/.config/openbox/rc.xml ${CBURL}/root/.config/openbox/rc.xml
 	sed -i 's|{{port}}|'${VNCPORT}'|g' ${HOME}/.config/openbox/autostart
 	mkdir ${HOME}/.icons
@@ -64,28 +57,22 @@ if [ $(tty) == /dev/tty1 ]; then
 				ln -s /usr/share/icons/Numix-Circle-Light ${HOME}/.icons/Numix-Circle-Light
 			
 			# Ardis
-				## svg icons -- won't work with Wbar atm ##
 				git clone https://github.com/KotusWorks/Ardis-icon-theme.git ${HOME}/.icons/Ardis-icon-theme
 				
 		# Themes
 			# Numix
 				apt-get -y install numix-gtk-theme
-					# backup ${HOME}/.gtkrc-2.0
 				curl -o ${HOME}/.gtkrc-2.0 ${CBURL}/root/.gtkrc-2.0
-					# backup ${HOME}/.config/gtk-3.0/settings.ini
 				curl --create-dirs -o ${HOME}/.config/gtk-3.0/settings.ini ${CBURL}/root/.config/gtk-3.0/settings.ini
 				
 		# Wallpapers
 			# Custom
 				curl https://dl.dropboxusercontent.com/s/ru883uwrnh98y7d/wallpapers.tar.gz | tar -zxvf - -C ${HOME}
-					# backup ${HOME}/.config/nitrogen/nitrogen.cfg
 				curl --create-dirs -o ${HOME}/.config/nitrogen/nitrogen.cfg ${CBURL}/root/.config/nitrogen/nitrogen.cfg
-					# backup ${HOME}/.config/nitrogen/bg-saved.cfg
 				curl -o ${HOME}/.config/nitrogen/bg-saved.cfg ${CBURL}/root/.config/nitrogen/bg-saved.cfg	
 
 	## Compositor ##
 		apt-get -y install compton
-			# backup ${HOME}/.config/compton.conf
 		curl -o ${HOME}/.config/compton.conf ${CBURL}/root/.config/compton.conf
 
 	## System Monitor ##
@@ -94,24 +81,22 @@ if [ $(tty) == /dev/tty1 ]; then
 		curl -o /etc/apt/sources.list.d/conky-manager.list ${CBURL}/etc/apt/sources.list.d/conky-manager.list
 		apt-get update
 		apt-get -y install conky-manager
-			# backup ${HOME}/.config/conky/.conkyrc
 		curl --create-dirs -o ${HOME}/.config/conky/.conkyrc ${CBURL}/root/.config/conky/.conkyrc
 
 	## Toolbar ##
 		apt-get -y install tint2
-			# backup ${HOME}/.config/tint2/tint2rc
 		curl --create-dirs -o ${HOME}/.config/tint2/tint2rc ${CBURL}/root/.config/tint2/tint2rc
 
 	## Launcher ##
 		curl -o ${HOME}/.install/libbamf3-2.deb http://mirrors.kernel.org/ubuntu/pool/main/b/bamf/libbamf3-2_0.5.2~bzr0+15.10.20150627.1-0ubuntu1_amd64.deb
 		dpkg -i ${HOME}/.install/libbamf3-2.deb
+		rm ${HOME}/.install/libbamf3-2.deb
 	
 		apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9E5DB0C8
 		curl -o /etc/apt/sources.list.d/plank.list ${CBURL}/etc/apt/sources.list.d/plank.list
 		apt-get update
 		apt-get -y install plank
 	
-		# apt-get -y -t experimental install plank
 		DCK1=${HOME}'/.config/plank/dock1'
 		DSRC=${CBURL}'/root/.config/plank/dock1'
 		PTHM=${HOME}'/.local/share/plank/themes/cloudbox'
@@ -141,26 +126,28 @@ if [ $(tty) == /dev/tty1 ]; then
 
 	## Terminal ##
 		apt-get -y install terminator
-			# backup ${HOME}/.config/terminator/config
 		curl --create-dirs -o ${HOME}/.config/terminator/config ${CBURL}/root/.config/terminator/config
 
 	## File Manager ##
 		mkdir --parents ${HOME}/.local/share
 		mkdir ${HOME}/Templates
 		apt-get -y install pcmanfm
-			# backup ${HOME}/.config/pcmanfm/default/pcmanfm.conf
 		curl --create-dirs -o ${HOME}/.config/pcmanfm/default/pcmanfm.conf ${CBURL}/root/.config/pcmanfm/default/pcmanfm.conf
-			# backup ${HOME}/.config/libfm/libfm.conf
 		curl --create-dirs -o ${HOME}/.config/libfm/libfm.conf ${CBURL}/root/.config/libfm/libfm.conf
 
 	## Text Editor ##
 		apt-get -y install geany
 		curl --create-dirs -o ${HOME}/.config/geany/colorschemes/zenburn.conf ${CBURL}/root/.config/geany/colorschemes/zenburn.conf
-			# backup ${HOME}/.config/geany/geany.conf
 		curl -o ${HOME}/.config/geany/geany.conf ${CBURL}/root/.config/geany/geany.conf
 
-	## Load final script && REBOOT ##
-		cat /dev/null > /var/log/syslog
-		# curl -o ${HOME}/.bashrc ${CBURL}/root/.bashrc
-		# reboot
+	## Clear history ##
+		clear_history
+
+	## Load proper .bashrc && .xinitrc ##
+		curl -o ${HOME}/.bashrc ${CBURL}/root/.bashrc
+		curl -o ${HOME}/.xinitrc ${CBURL}/root/.xinitrc
+
+	## REBOOT ##
+		echo 'Rebooting in 5 sec ...' && sleep 5
+		reboot
 fi
