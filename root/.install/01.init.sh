ed813e95b913895682dafb0abdbe5e192ec3b9c4
kinologik <martin@kinologik.com>
2015-07-22 12:12:30 -0400

    Update 01.init.sh

--- root/.install/01.init.sh
+++ root/.install/01.init.sh
@@ -2,12 +2,11 @@
 
 source ${HOME}/.scripts/lib.sh
 
-function host_tz
-{
+function host_tz {
 		backup /etc/hostname
 		copy /etc/hosts
 		backup /etc/timezone
-		backup /etc/localtime
+		rm /etc/localtime
 		
 	echo -e ${HNAME} > /etc/hostname
 	sed -i 's|127.0.0.1 localhost|127.0.0.1 localhost '${HNAME}'|g' /etc/hosts
@@ -16,16 +15,14 @@ function host_tz
 	ln -s /usr/share/zoneinfo/${TZONE} /etc/localtime
 }
 
-function update_sources
-{
+function update_sources {
 		backup /etc/apt/sources.list
 	curl -o /etc/apt/sources.list ${CBURL}/etc/apt/sources.${OSDIST}.list
 	apt-get update
 		if [ $? != 0 ]; then echo "update failed..."; exit; fi
 }
 
-function update_upgrade
-{
+function update_upgrade {
 		backup /etc/apt/listchanges.conf
 	curl -o /etc/apt/listchanges.conf ${CBURL}/etc/apt/listchanges.conf
 	apt-get -y dist-upgrade
@@ -33,8 +30,7 @@ function update_upgrade
 	apt-get -y autoremove
 }
 
-function create_swapfile
-{
+function create_swapfile {
 	touch /swapfile
 	chmod 600 /swapfile
 	dd if=/dev/zero of=/swapfile bs=1M count=2048
@@ -45,11 +41,16 @@ function create_swapfile
 	sysctl -w vm.swappiness=30
 }
 
-function update_ssh
-{
+function update_ssh {
 		backup /etc/ssh/sshd_config
 	curl -o /etc/ssh/sshd_config ${CBURL}/etc/ssh/sshd_config
 	sed -i 's|{{port}}|'${SSHPORT}'|g' /etc/ssh/sshd_config
+}
+
+function self_ssl {
+	mkdir --parents /etc/ssl/${DOMAIN}/certs && mkdir --parents /etc/ssl/${DOMAIN}/private
+	SSLSUBJ='/C='${CERTC}'/ST='${CERTST}'/L='${CERTL}'/O='${CERTO}'/CN=*.'${DOMAIN}
+	openssl req -x509 -sha256 -nodes -days 5480 -newkey rsa:4096 -out /etc/ssl/${DOMAIN}/certs/${DOMAIN}.pem -keyout /etc/ssl/${DOMAIN}/private/${DOMAIN}.key -utf8 -subj "${SSLSUBJ}"
 }
 
 if [ $(tty) == /dev/tty1 ]; then
@@ -68,6 +69,9 @@ if [ $(tty) == /dev/tty1 ]; then
 	## SSH port settings ##
 		update_ssh
 
+	## Create self-signed certificate ##
+		self_ssl
+		
 	## Clear history ##
 		clear_history
 	
